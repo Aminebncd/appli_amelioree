78f0de78be3ecf747973a513f6694f3f8716fd2b
Aminebncd <amine.bounachada@hotmail.com>
2024-01-10 17:01:56 +0100

    commit init

---
+++ appli/index.php
@@ -0,0 +1,42 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Ajout produit</title>
+</head>
+<body>
+    <h1>Ajouter un produit</h1>
+    <form action="traitement.php" method="post">
+    <p>
+        <label>
+            Nom du produit :
+            <input type="text" name="name">
+        </label>
+        </p>
+    <p>
+        <label>
+            Prix du produit :
+            <input type="text" name="price">
+        </label>
+    </p>
+    <p>
+        <label>
+            Quantitée desirée :
+            <input type="text" name="qtt">
+        </label>
+    </p>
+    <p>
+        <input type="submit" name="submit" value="Ajouter le produit">
+    </p>
+
+</form>
+
+</body>
+</html>
+
---
+++ appli/recap.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recapitulatif des produits</title>
+</head>
+<body>
+    <?php 
+
+    // var_dump($_SESSION); 
+    if (!isset($_SESSION['products'])) || empty($_SESSION['products']))
+    
+    ?>
+</body>
+</html>
